14ee26cba66d8125e313e72c38e7db676b61c932
Lalo64GG <223257@ids.upchiapas.edu.mx>
2023-11-23 18:11:13 -0600

    Creacion de varios componentes

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Layout } from './Components/Organims/Layout'
+
+//? Creación de las rutas.
+//* Utilizzamos createBrowserRouter de react router para generar el enrutamiento.
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <Layout/>, //? Aquí va el componente Layout
+  errorElement: <h2>Error</h2>, //? Aqui va el elemeto de error
+  children: [
+    //? Todo lo que esté dentro de este "children" compartirá el mismo diseño (Layout)
+    {
+      index: true,
+      element: <h1>Componente</h1>,
+    }
+  ]
+}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      {/* //? Provider de react-router-dom que utiliza el router creado */}
+    <RouterProvider router = { router } />
+
   </React.StrictMode>,
 )
